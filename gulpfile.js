9d1bcca95777ad9eb1d7e7ef62f4a532bdbea440
Alex Baker <al3xb4k3r@gmail.com>
2016-09-25 19:59:55 -0600

    Added sass gulp task and normalize.css.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,25 @@ const nunjucks = require('gulp-nunjucks');
 const markdown = require('gulp-markdown');
 const browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // Stuff here
+// Directory names
+const srcRoot = 'src/';
+const distRoot = 'dist/';
+const assetsDir = 'assets/';
+const stylesDir = 'scss/';
+
+// Paths
+const srcAssestsPath = srcRoot + assetsDir;
+const srcStylesPath = srcAssestsPath + stylesDir;
+const distAssetsPath = distRoot + assetsDir;
+
+
+gulp.task('sass', function () {
+  var options = {
+    outputStyle: 'compressed'
+  };
+  return gulp.src(srcStylesPath + 'main.scss')
+    .pipe(sass(options).on('error', sass.logError))
+    .pipe(gulp.dest(distAssetsPath));
 });
+
+gulp.task('default', ['sass']);
