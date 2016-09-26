c27ec3cae460030960c93a4a81f7fef6f1715020
Alex Baker <al3xb4k3r@gmail.com>
2016-09-25 20:14:42 -0600

    Added gulp revision and file clean plugin. Updated sass task to add revision hash to CSS output.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const sass = require('gulp-sass');
+const rev = require('gulp-rev');
 const nunjucks = require('gulp-nunjucks');
 const markdown = require('gulp-markdown');
 const browserSync = require('browser-sync').create();
@@ -16,13 +18,19 @@ const srcStylesPath = srcAssestsPath + stylesDir;
 const distAssetsPath = distRoot + assetsDir;
 
 
+gulp.task('clean', function () {
+    return gulp.src(distRoot, {read: false})
+        .pipe(clean());
+});
+
 gulp.task('sass', function () {
   var options = {
     outputStyle: 'compressed'
   };
   return gulp.src(srcStylesPath + 'main.scss')
     .pipe(sass(options).on('error', sass.logError))
+    .pipe(rev())
     .pipe(gulp.dest(distAssetsPath));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['clean', 'sass']);
