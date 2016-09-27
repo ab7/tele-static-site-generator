9e9f01d4ea5abba6c2172ad87a469909d62add98
Alex Baker <al3xb4k3r@gmail.com>
2016-09-26 21:48:47 -0600

    Swapped out nunjucks dependency and set up basic template compilation task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,34 @@ const gulp = require('gulp');
 const clean = require('gulp-clean');
 const sass = require('gulp-sass');
 const rev = require('gulp-rev');
-const nunjucks = require('gulp-nunjucks');
+const nunjucksRender = require('gulp-nunjucks-render');
 const markdown = require('gulp-markdown');
+
 const browserSync = require('browser-sync').create();
+
 
 // Directory names
 const srcRoot = 'src/';
 const distRoot = 'dist/';
 const assetsDir = 'assets/';
 const stylesDir = 'scss/';
+const templatesDir = 'templates/';
+const pagesDir = 'pages/';
 
 // Paths
 const srcAssestsPath = srcRoot + assetsDir;
 const srcStylesPath = srcAssestsPath + stylesDir;
 const distAssetsPath = distRoot + assetsDir;
+const templatesPath = srcRoot + templatesDir;
+const pagesPath = srcRoot + pagesDir;
 
 
 gulp.task('clean', function () {
-    return gulp.src(distRoot, {read: false})
-        .pipe(clean());
+  return gulp.src(distRoot + '*', {read: false})
+    .pipe(clean());
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', ['clean'], function () {
   var options = {
     outputStyle: 'compressed'
   };
@@ -33,4 +39,10 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(distAssetsPath));
 });
 
-gulp.task('default', ['clean', 'sass']);
+gulp.task('compile-templates', ['clean'], function() {
+  return gulp.src(pagesPath + '*.html')
+    .pipe(nunjucksRender())
+    .pipe(gulp.dest(distRoot))
+});
+
+gulp.task('default', ['compile-templates', 'sass']);
